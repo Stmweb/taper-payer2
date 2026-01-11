8a911b8015603604789f6a95471cb94f7a3272ad
Ficus Aurea LLC <ficusaureallc@gmail.com>
2026-01-11 06:08:01 +0000

    File changes

--- src/components/taperpayer/TaperPayerLogo.jsx
+++ src/components/taperpayer/TaperPayerLogo.jsx
@@ -6,6 +6,7 @@ export default function TaperPayerLogo({ className = "", height = "h-8" }) {
       src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695c31d62d68bbb4ef8cc5b3/ea493ee34_ChatGPTImageJan112026at01_01_45AM.png"
       alt="Taper Payer"
       className={`${height} w-auto ${className}`}
+      style={{ mixBlendMode: 'multiply' }}
     />
   );
 }

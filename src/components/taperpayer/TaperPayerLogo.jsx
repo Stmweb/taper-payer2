6ef1b52d01b47d22d57342fcf1a5c275b40702df
Ficus Aurea LLC <ficusaureallc@gmail.com>
2026-01-11 06:04:53 +0000

    File changes

--- src/components/taperpayer/TaperPayerLogo.jsx
+++ src/components/taperpayer/TaperPayerLogo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TaperPayerLogo({ className = "", height = "h-8" }) {
   return (
     <img 
-      src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695c31d62d68bbb4ef8cc5b3/899b5ea8a_ChatGPTImageJan5202603_27_37PM.png"
+      src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695c31d62d68bbb4ef8cc5b3/ea493ee34_ChatGPTImageJan112026at01_01_45AM.png"
       alt="Taper Payer"
       className={`${height} w-auto ${className}`}
     />

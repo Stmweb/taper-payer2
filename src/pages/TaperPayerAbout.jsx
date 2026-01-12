b44539ac2fbc3a421b05e9bb89962a37f959712e
Ficus Aurea LLC <ficusaureallc@gmail.com>
2026-01-12 02:11:37 +0000

    File changes

--- src/pages/TaperPayerAbout.jsx
+++ src/pages/TaperPayerAbout.jsx
@@ -54,7 +54,7 @@ export default function TaperPayerAbout() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-12">
             <Link to={createPageUrl('TaperPayerHome')}>
-              <TaperPayerLogo height="h-10" />
+              <TaperPayerLogo height="h-34" />
             </Link>
             <div className="hidden md:flex items-center gap-8">
               <Link to={createPageUrl('TaperPayerHome')} className="text-gray-600 hover:text-blue-600 transition-colors font-medium">

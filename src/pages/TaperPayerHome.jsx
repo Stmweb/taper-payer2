ae0536269c061f7cd6426059110922f28f2cc557
Ficus Aurea LLC <ficusaureallc@gmail.com>
2026-01-13 02:43:21 +0000

    File changes

--- src/pages/TaperPayerHome.jsx
+++ src/pages/TaperPayerHome.jsx
@@ -30,7 +30,7 @@ export default function TaperPayerHome() {
             <img 
               src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/695c31d62d68bbb4ef8cc5b3/ab6777bfd_ChatGPTImageJan11202608_58_36PM.png"
               alt="Taper Payer"
-              className="h-20 md:h-32 w-auto"
+              className="h-30 md:h-32 w-auto"
             />
             
             {/* Desktop Navigation */}

91ae09a0b652699d475a58666503cca2a36f2e19
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-12 21:31:46 +0000

    File changes

--- src/pages/TaperPayerHome.jsx
+++ src/pages/TaperPayerHome.jsx
@@ -205,7 +205,7 @@ export default function TaperPayerHome() {
         </div>
 
         <div className="container mx-auto relative z-10">
-        <div className="grid lg:grid-cols-2 gap-12 items-center">
+          <div className="grid lg:grid-cols-2 gap-12 items-center">
           <motion.div
             initial={{ opacity: 0, x: -30 }}
             animate={{ opacity: 1, x: 0 }}

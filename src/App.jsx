f10da0b4a4016336e75f2a6e4260261f601d8ed4
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-12 23:06:29 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import TaperPayerTerms from './pages/TaperPayerTerms';
 import TaperPayerPrivacy from './pages/TaperPayerPrivacy';
 import TaperPayerCookies from './pages/TaperPayerCookies';
 import TaperPayerMarketing from './pages/TaperPayerMarketing';
+import TaperPayerBusiness from './pages/TaperPayerBusiness';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];

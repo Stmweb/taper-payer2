829bb97240568d3543f9cd7787043d0bd10774f8
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-14 17:42:33 +0000

    File changes

--- src/pages/TaperPayerTopUp.jsx
+++ src/pages/TaperPayerTopUp.jsx
@@ -245,7 +245,7 @@ export default function TaperPayerTopUp() {
       </section>
 
       {/* Taper Connect Sheet */}
-      {showTaperConnect && (
+      {showTaperConnect && createPortal(
         <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
           <motion.div
             initial={{ opacity: 0 }}

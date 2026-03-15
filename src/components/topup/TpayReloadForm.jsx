50930372e7760d7bd5c9c3251eb079499ff9af80
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-15 21:12:25 +0000

    File changes

--- src/components/topup/TpayReloadForm.jsx
+++ src/components/topup/TpayReloadForm.jsx
@@ -322,15 +322,7 @@ export default function TpayReloadForm() {
               />
             )}
 
-            {paymentMethod === 'rsa' && selectedCountry?.iso === 'HT' && (
-              <RSAPaymentForm
-                phoneNumber={`${selectedCountry?.dial}${phoneNumber}`}
-                amount={amount}
-                operatorId={selectedOperator?.operatorId || selectedOperator?.id}
-                countryCode={selectedCountry?.iso}
-                onSuccess={() => setSuccess(true)}
-              />
-            )}
+
 
             {paymentMethod === 'card' && (
               <Button

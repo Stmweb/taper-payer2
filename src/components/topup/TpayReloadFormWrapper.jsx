cdc077377ca1a1bbb85017a37b1ebf497b339cc5
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-16 12:46:17 +0000

    File changes

--- src/components/topup/TpayReloadFormWrapper.jsx
+++ src/components/topup/TpayReloadFormWrapper.jsx
@@ -1,37 +1,6 @@
 import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import { base44 } from '@/api/base44Client';
-import TpayReloadForm from './TpayReloadForm';
-
-let stripePromise;
-
-async function getStripePromise() {
-  if (!stripePromise) {
-    try {
-      const res = await base44.functions.invoke('getStripeKey', {});
-      stripePromise = loadStripe(res.data.publicKey);
-    } catch (e) {
-      console.error('Failed to load Stripe key:', e);
-    }
-  }
-  return stripePromise;
-}
+import DToneTopUpForm from './DToneTopUpForm';
 
 export default function TpayReloadFormWrapper() {
-  const [stripePromiseValue, setStripePromiseValue] = React.useState(null);
-
-  React.useEffect(() => {
-    getStripePromise().then(setStripePromiseValue);
-  }, []);
-
-  if (!stripePromiseValue) {
-    return <div className="text-center py-8">Loading payment system...</div>;
-  }
-
-  return (
-    <Elements stripe={stripePromiseValue}>
-      <TpayReloadForm />
-    </Elements>
-  );
+  return <DToneTopUpForm />;
 }

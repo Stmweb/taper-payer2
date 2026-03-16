53eb7f47ae226833336f278dcfc78fd5243af310
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-16 13:22:14 +0000

    File changes

--- src/components/topup/TaperConnectFormWrapper.jsx
+++ src/components/topup/TaperConnectFormWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { base44 } from '@/api/base44Client';

170368e7f7dbb49c42f6e58bcc9aadd6e305ddf7
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-16 22:52:20 +0000

    File changes

--- functions/dtoneTopUp.ts
+++ functions/dtoneTopUp.ts
@@ -16,7 +16,10 @@ Deno.serve(async (req) => {
     if (action === 'getProducts') {
       const { countryIso } = body;
       // DTone uses ISO 3166-1 alpha-3 codes
-      const iso2To3 = { HT: 'HTI', IN: 'IND', PH: 'PHL', NG: 'NGA', KE: 'KEN', GH: 'GHA', MX: 'MEX', BR: 'BRA', SN: 'SEN', AO: 'AGO', DO: 'DOM', CL: 'CHL', MA: 'MAR', JM: 'JAM' };
+      const iso2To3 = { 
+        HT: 'HTI', IN: 'IND', PH: 'PHL', NG: 'NGA', KE: 'KEN', GH: 'GHA', MX: 'MEX', BR: 'BRA', SN: 'SEN', AO: 'AGO', DO: 'DOM', CL: 'CHL', MA: 'MAR', JM: 'JAM',
+        US: 'USA', CA: 'CAN', TZ: 'TZA', UG: 'UGA', ET: 'ETH', RW: 'RWA', CI: 'CIV', ML: 'MLI', BF: 'BFA', CM: 'CMR', BJ: 'BEN', TD: 'TCD', CG: 'COG', GA: 'GAB', ZA: 'ZAF', ZW: 'ZWE', ZM: 'ZMB', BW: 'BWA', NA: 'NAM', MU: 'MUS', TN: 'TUN', DZ: 'DZA', EG: 'EGY', PK: 'PAK', BD: 'BGD', LK: 'LKA', ID: 'IDN', TH: 'THA', VN: 'VNM', MY: 'MYS', SG: 'SGP', SA: 'SAU', AE: 'ARE', QA: 'QAT', KW: 'KWT', BH: 'BHR', OM: 'OMN', TT: 'TTO', BB: 'BRB', HN: 'HND', SV: 'SLV', GT: 'GTM', NI: 'NIC', CR: 'CRI', PA: 'PAN', CO: 'COL', VE: 'VEN', PE: 'PER', EC: 'ECU', BO: 'BOL', AR: 'ARG', PY: 'PRY', UY: 'URY'
+      };
       const iso3 = iso2To3[countryIso] || countryIso;
       const res = await fetch(
         `${BASE_URL}/products?country_iso_code=${iso3}&type=FIXED_VALUE_RECHARGE&per_page=100`,

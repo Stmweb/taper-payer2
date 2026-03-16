b2e1db18532fc24494aa5fb94e21b7e85bf7cfa9
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-16 22:39:53 +0000

    File changes

--- src/pages/TaperPayerTopUp.jsx
+++ src/pages/TaperPayerTopUp.jsx
@@ -409,7 +409,7 @@ export default function TaperPayerTopUp() {
               className="absolute top-4 right-4 p-2 hover:bg-gray-100 rounded-full z-10"
             >✕</button>
             <div className="p-6 pt-12">
-              <TaperConnectFormWrapper />
+              <TaperConnectFormWrapper initialCountry={selectedCountry} />
             </div>
           </motion.div>
         </div>,

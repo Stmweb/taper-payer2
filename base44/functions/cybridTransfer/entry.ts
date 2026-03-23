2fb31ebd6943d11b4d9b4612176069b55f57eeb1
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-23 14:25:55 +0000

    File changes

--- base44/functions/cybridTransfer/entry.ts
+++ base44/functions/cybridTransfer/entry.ts
@@ -86,6 +86,7 @@ Deno.serve(async (req) => {
 
       const customer = await cybridApi(token, 'POST', '/api/customers', {
         type: 'individual',
+        bank_guid: CYBRID_BANK_GUID,
         name: { first: firstName, last: lastName },
         email_address: email,
       });

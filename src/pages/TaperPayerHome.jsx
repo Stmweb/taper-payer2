bfa5a8e6cac9829fe858ffe01ff0496166fabd34
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-24 13:35:44 +0000

    File changes

--- src/pages/TaperPayerHome.jsx
+++ src/pages/TaperPayerHome.jsx
@@ -81,6 +81,7 @@ export default function TaperPayerHome() {
    const [showReloadForm, setShowReloadForm] = useState(false);
    const [showTaperConnect, setShowTaperConnect] = useState(false);
    const [showTransferModal, setShowTransferModal] = useState(false);
+   const [showHaitiModal, setShowHaitiModal] = useState(false);
    const [showComingSoon, setShowComingSoon] = useState(false);
    const [showSignupModal, setShowSignupModal] = useState(false);
 

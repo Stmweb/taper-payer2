4fd6b2002887b445bfccac1f31700da907f72711
Ficus Aurea <ficusaureallc@gmail.com>
2026-03-24 03:29:59 +0000

    File changes

--- base44/functions/cybridTransfer/entry.ts
+++ base44/functions/cybridTransfer/entry.ts
@@ -579,7 +579,7 @@ Deno.serve(async (req) => {
           { type: 'customer', guid: customerGuid, amount: amountCents },
         ],
         destination_participants: [
-          { type: 'counterparty', guid: counterpartyExternalBankAccountGuid, amount: amountCents },
+          { type: 'external_bank_account', guid: counterpartyExternalBankAccountGuid, amount: amountCents },
         ],
       });
       return Response.json({ quote, remittance });
